b0d6860a70b168caf6f9566063cfb8ed57168642
Ibnu adam <111300374+ibnuadam26@users.noreply.github.com>
2023-08-01 08:25:45 +0700

    31 Jul 23

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let a= `${1+2}`
 document.getElementById('js').innerText = a;
 console.log(a);
+
+let mhs=`Andi`
+let mhs2=`Budi`
+let names=`The name are ${mhs} and ${mhs2}`
+document.getElementById('js2').innerText = names;
+console.log(names);
